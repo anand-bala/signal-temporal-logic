3b425cc996213ec37c54acfc56d42baea25b4a52
Anand Balakrishnan <anandbala1597@gmail.com>
2020-07-26 23:51:08 -0700

    [skip azp] Add a python version for Travis

--- build.sh
+++ build.sh
@@ -5,7 +5,11 @@ set -euo pipefail
 mkdir -p build && cd build
 
 # Configure
-cmake -DSIGNALTL_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DSIGNALTL_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug \
+  -DPYTHON_EXECUTABLE=$(python-config --prefix)/bin/python3.6 \
+  -DPYTHON_LIBRARY=$(python-config --prefix)/lib/libpython3.6m.so \
+  -DPYTHON_INCLUDE_DIR=$(python-config --prefix)/include/python3.6m \
+  ..
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
 cmake --build . --config Debug -- -j $(nproc)
 # Test
